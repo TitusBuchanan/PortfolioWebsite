8cb8e74666a77095c911448e434ad7ad0973120d
titus buchanan <titusbuchananjr@gmail.com>
2020-05-08 03:15:05 -0400

    Personal Website made using React

---
+++ src/components/landingpage.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+
+
+class Landing extends Component {
+    render(){
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https:cdn4.iconfinder.com/data/icons/business-avatar-3/64/Business-Man-Avatar_2-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                       
+                        <div className="banner-text">
+                            <h1 className="header">Full Stack Web Developer</h1>
+
+                            <hr/>
+
+                            <p> HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB | Git </p>  
+
+                            <div className="social-links">
+                               {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/titusbuchanan/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="https://github.com/TitusBuchanan" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                                {/* FreeCodeCamp */}
+                                <a href="https://www.freecodecamp.org/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                </a>
+                                {/* Twitter */}
+                                <a href="https://www.instagram.com/prettymofotitus/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-instagram" aria-hidden="true" />
+                                </a>
+                            </div>  
+                            
+                        </div>
+                    </Cell>
+                </Grid>
+                
+            </div>
+        )
+    }
+};
+
+
+export default Landing;
---
+++ src/components/projects.js
@@ -0,0 +1,215 @@
+import React, {Component} from 'react'
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions,Button,CardMenu,IconButton, CardText } from 'react-mdl';
+
+class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab:0 };
+    }
+    
+    
+   toggleCategories() {
+    if(this.state.activeTab === 0){
+        return (
+            
+            <div className="projects-grid">
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://reactjs.org/logo-og.png) center/cover'}}></CardTitle>
+                        <CardText >
+                            My First React Project
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/ReactProjects">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://reactjs.org/logo-og.png) center/cover'}}></CardTitle>
+                        <CardText >
+                            REACT project making a To-Do Lst.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/ToDoReact">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://reactjs.org/logo-og.png) center/cover'}}></CardTitle>
+                        <CardText >
+                            Code for this website which was made with only REACT!
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/ReactProjects">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+           </div>
+    
+        )
+            
+                
+
+
+            
+        
+           
+           
+
+           
+    } else if(this.state.activeTab === 1) {
+        return (
+            <div className="projects-grid">
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://miro.medium.com/max/1440/1*ahpxPO0jLGb9EWrY2qQPhg.jpeg) center/cover'}}></CardTitle>
+                        <CardText >
+                           Miscellaneous JavaScript Algorithims 
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/fcc">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://miro.medium.com/max/1440/1*ahpxPO0jLGb9EWrY2qQPhg.jpeg) center/cover'}}></CardTitle>
+                        <CardText >
+                        Javascript project using passport, passport local, bcrypt and other dependencies displaying my knowledge of these dependencies and how to incoperate them in applications
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/Passport">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+                <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://miro.medium.com/max/1440/1*ahpxPO0jLGb9EWrY2qQPhg.jpeg) center/cover'}}></CardTitle>
+                        <CardText >
+                        JavaScript HackerRank problem completed using JS
+                        </CardText>
+                        <CardActions border>
+                            <Button colored href="https://github.com/TitusBuchanan/HackerRank">Github</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+           </div>
+        )
+    } else if(this.state.activeTab === 2) {
+        return (
+            <div className="projects-grid">
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://i.pcmag.com/imagery/reviews/02Q6yxveinggAu3PomearaV-7..v_1569481734.jpg) center/cover'}}></CardTitle>
+                    <CardText >
+                        Agile Men Stack Project using MongoDB,Express, And NodeJS
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan/NewApiTeamProject">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://i.pcmag.com/imagery/reviews/02Q6yxveinggAu3PomearaV-7..v_1569481734.jpg) center/cover'}}></CardTitle>
+                    <CardText >
+                    Scrum Men Stack Project using MongoDB,Express, And NodeJS
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan/menStack-1">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://i.pcmag.com/imagery/reviews/02Q6yxveinggAu3PomearaV-7..v_1569481734.jpg) center/cover'}}>React Project #3</CardTitle>
+                    <CardText >
+                        Solo Men Stack Project using MongoDB,Express, And NodeJS
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan/ReactProjects">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+       </div>
+        )
+    }else if(this.state.activeTab === 3) {
+        return (
+            <div className="projects-grid">
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://hackernoon.com/drafts/ar1wv331n.png) center/cover'}}></CardTitle>
+                    <CardText >
+                        Coming Soon....
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://hackernoon.com/drafts/ar1wv331n.png) center/cover'}}></CardTitle>
+                    <CardText >
+                    Coming Soon....
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+            <Card shadow={5} style={{ minWidth:'450', margin:'auto' }}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://hackernoon.com/drafts/ar1wv331n.png) center/cover'}}>React Project #3</CardTitle>
+                    <CardText >
+                    Coming Soon....
+                    </CardText>
+                    <CardActions border>
+                        <Button colored href="https://github.com/TitusBuchanan">Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+            </Card>
+       </div>
+        )
+    }
+   }
+    
+    render(){
+        return(
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId })} ripple>
+                <Tab>React</Tab>
+                <Tab>JavaScript</Tab>
+                <Tab>MongoDB</Tab>
+                <Tab>MERN</Tab>
+                </Tabs>
+
+                
+                    <Grid >
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+                    
+                
+            </div>
+        )
+    }
+};
+
+
+export default Projects;
