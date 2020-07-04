0775bcd49c47d30a561c452869da1d545f3819e4
titus buchanan <titusbuchananjr@gmail.com>
2020-07-04 05:00:31 -0400

    deleted about me experience

--- src/components/resume.js
+++ src/components/resume.js
@@ -21,7 +21,7 @@ class Resume extends Component {
                         <h2 style={{paddingTop:'2em'}}>Titus Buchanan Jr</h2>
                         <h4 style={{color:'grey'}}>Full Stack Web Developer</h4>
                         <hr style={{borderTop:'3px solid #833fb2', width:'50%'}}></hr>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised </p>
+                        <p></p>
                         <hr style={{borderTop:'3px solid #833fb2', width:'50%'}}></hr>
                         <h5>Address</h5>
                         <p>91 Ring Street #3, Providence, RI 02909</p>
